8b2990081f5f29d5922aa2a49e12727bcc1801d2
Sara <sara82397@gmail.com>
2021-01-25 19:32:47 -0300

    implementando activity e comportamentos

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.agenda;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Sara Felix", Toast.LENGTH_SHORT).show();
     }
 }
