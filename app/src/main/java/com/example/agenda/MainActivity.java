170bca3d194bda0e0daf6e44840455b71ccd1100
Sara <sara82397@gmail.com>
2021-01-25 19:56:18 -0300

    implementando o layout da activity

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -14,8 +14,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Sara Felix");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
     }
 }
