e7e10eac8653924fde09242d1cb8d3f894614307
Sara <sara82397@gmail.com>
2021-01-26 12:10:23 -0300

    add title

--- app/src/main/java/com/example/agenda/MainActivity.java
+++ app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.agenda;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -12,12 +13,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>
                 (Arrays.asList("Sara", "Fran", "Jose"));
        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
