46cd43529f5ac38078127f55062be58144cd23a2
Sara <sara82397@gmail.com>
2021-01-26 12:56:05 -0300

    implementando layout do form

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,28 +1,28 @@
-package com.example.agenda;
+package com.example.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import com.example.agenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         setTitle("Lista de Alunos");
         List<String> alunos = new ArrayList<>
                 (Arrays.asList("Sara", "Fran", "Jose"));
-       ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+       ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
        listaDeAlunos.setAdapter(new ArrayAdapter<String>(
                this,
                android.R.layout.simple_list_item_1,
